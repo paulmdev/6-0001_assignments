e037aaa992e4bd19a0d21d7490f5fad880733ebf
Paul Muad'Dev <97257597+paulmdev@users.noreply.github.com>
2022-01-24 16:36:10 -0500

    Create tests for update_hand

--- ps3/ps3_test.py
+++ ps3/ps3_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ps3 import get_word_score
+from ps3 import get_word_score, update_hand
 
 
 class Tests(unittest.TestCase):
@@ -10,3 +10,42 @@ class Tests(unittest.TestCase):
                  ("fork", 7): 209, ("FORK", 4): 308}
         for word, n in words.keys():
             self.assertEqual(get_word_score(word, n), words[(word, n)])
+
+
+class UpdateHandTests(unittest.TestCase):
+    def test_update_hand_1(self):
+        original_hand = {'a': 1, 'q': 1, 'l': 2, 'm': 1, 'u': 1, 'i': 1}
+        original_hand_copy = original_hand.copy()
+        word = "quail"
+
+        new_hand = update_hand(original_hand_copy, word)
+        expected_new_hand = {'l': 1, 'm': 1}
+
+        self.assertDictEqual(new_hand, expected_new_hand)
+
+        # Test it doesn't modify the hand
+        self.assertDictEqual(original_hand, original_hand_copy)
+
+    def test_update_hand_2(self):
+        original_hand = {'e': 1, 'v': 2, 'n': 1, 'i': 1, 'l': 2}
+        original_hand_copy = original_hand.copy()
+        word = "Evil"
+
+        new_hand = update_hand(original_hand_copy, word)
+        expected_new_hand = {'v': 1, 'n': 1, 'l': 1}
+
+        self.assertDictEqual(new_hand, expected_new_hand)
+
+        self.assertDictEqual(original_hand, original_hand_copy)
+
+    def test_update_hand_3(self):
+        original_hand = {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+        original_hand_copy = original_hand.copy()
+        word = "HELLO"
+
+        new_hand = update_hand(original_hand_copy, word)
+        expected_new_hand = {}
+
+        self.assertDictEqual(new_hand, expected_new_hand)
+
+        self.assertDictEqual(original_hand, original_hand_copy)
